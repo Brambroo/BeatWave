5ef38eee87b1e9a1d6c30ac7db376323f4628c39
Brambroo <cclarke679@yahoo.com>
2019-03-25 19:08:06 -0400

    Updated despawner

---
+++ Assets/Scripts/DespawnEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnEnemy : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("basic"))
+        {
+            Destroy(collision.gameObject);
+            GameObject.Find("Player").GetComponent<HealthManager>().subtractHealth();
+        }
+    }
+}
